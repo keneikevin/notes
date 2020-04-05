fd17fc601a844d5a333e8a5e68aadf2ac67ab208
keneikevin <kevinkenei9@gmail.com>
2020-04-05 20:16:19 +0300

    Recycler view and viewmodel

--- app/src/main/java/com/example/notes/data/NoteDatabase.kt
+++ app/src/main/java/com/example/notes/data/NoteDatabase.kt
@@ -1,36 +1,57 @@
 package com.example.notes.data
 
 import android.content.Context
+import android.os.AsyncTask
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Database(entities = [Note::class], version = 1)
 abstract class NoteDatabase : RoomDatabase() {
 
     abstract fun noteDao(): NoteDao
 
-    companion object {
 
-        @Volatile
-        private var INSTANCE: NoteDatabase? = null
+    companion object {
+        private var instance: NoteDatabase? = null
 
         fun getInstance(context: Context): NoteDatabase {
-            synchronized(this) {
-                var instance = INSTANCE
-
-                if (instance == null) {
+            if (instance == null) {
+                synchronized(NoteDatabase::class) {
                     instance = Room.databaseBuilder(
                             context.applicationContext,
-                            NoteDatabase::class.java,
-                            "sleep_history_database"
+                            NoteDatabase::class.java, "notes_database"
                         )
                         .fallbackToDestructiveMigration()
+                        .addCallback(roomCallback)
                         .build()
-                    INSTANCE = instance
                 }
-                return instance
+            }
+            return instance!!
+        }
+
+        fun destroyInstance() {
+            instance = null
+        }
+
+        private val roomCallback = object : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                PopulateDbAsyncTask(instance)
+                    .execute()
             }
         }
+
     }
+    class PopulateDbAsyncTask(db: NoteDatabase?) : AsyncTask<Unit, Unit, Unit>() {
+        private val noteDao = db?.noteDao()
+
+        override fun doInBackground(vararg p0: Unit?) {
+            noteDao?.insert(Note(1,"Title 1", "description 1"))
+            noteDao?.insert(Note(2,"Title 2", "description 2"))
+            noteDao?.insert(Note(3,"Title 3", "description 3"))
+        }
+    }
+
 }
